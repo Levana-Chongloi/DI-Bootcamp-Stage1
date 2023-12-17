f76f9a3ee108c31b1480f3949eb3c053fdb5010e
Levana <levanacloe@gmail.com>
2023-12-17 18:11:48 +0200

    week3

---
+++ Week3/day2/Day2-Daily-Challenge/Exercise-1/script.js
@@ -0,0 +1,31 @@
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+//1. Remove Banana from the array.
+
+fruits.shift();
+
+//2. Sort the array in alphabetical order.
+
+fruits.sort();
+
+// 3. Add “Kiwi” to the end of the array.
+
+fruits.push("Kiwi");
+
+
+// 4. Remove “Apples” from the array. Don’t use the same method as in part 1.
+
+fruits.splice(0,1);
+
+
+
+// 5. Sort the array in reverse order
+fruits.reverse();
+
+console.log(fruits);
+
+
+
+
+
+
